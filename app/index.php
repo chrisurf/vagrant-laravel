1417e4ecfabc5d7ea70d39f68ad9bdcbbfbc2ba8
chrisurf <chrisurf@users.noreply.github.com>
2021-08-08 10:14:13 +0200

    updates doc

--- app/index.php
+++ app/index.php
@@ -4,10 +4,13 @@
 <head>
   <meta charset="utf-8">
   <title>Vagrant Laravel Sandbox</title>
+  <meta name="description" content="A Vagrant Laravel Sandbox find more information on: https://github.com/chrisurf/vagrant-laravel">
+  <meta name="keywords" content="Ubuntu, PHP, Laravel">
+  <meta name="author" content="Chris Oguntolu">
 </head>
 
 <body>
-  <h1>Web Application</h1>
+  <h1>PHP Enviroment</h1>
   <?php phpinfo(); ?>
 </body>
 
